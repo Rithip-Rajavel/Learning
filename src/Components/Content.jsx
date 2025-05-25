dfe1b71311836d2b34cedddd3557b8fdf638c7fc
Rithip-Rajavel <rithipcegian@gmail.com>
2025-05-25 16:29:22 +0530

    basic todolist complete

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import styled from "styled-components";
 import { useState } from "react";
 import Shop from "./Shop";
+import { FaTrashCan } from "react-icons/fa6";
+import { FaEdit } from "react-icons/fa";
+import { IoIosAddCircle } from "react-icons/io";
 
 let handingStyle = {
   backgroundColor: "red",
@@ -35,13 +38,60 @@ function HandleButton1(name) {
 
 function Content() {
   const [name, setName] = useState("Archana");
-
   const [items, setItems] = useState([
     { id: 1, label: "HTML & CSS", checked: true },
     { id: 2, label: "JavaSript", checked: true },
     { id: 3, label: "React", checked: false },
     { id: 4, label: "Nodejs", checked: false },
   ]);
+
+  const [text, setText] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
+  const [currentElementID, setElementID] = useState(null);
+  const handlechecked = (id) => {
+    const newItems = items.map((item) => {
+      return item.id === id ? { ...item, checked: !item.checked } : item;
+    });
+    setItems(newItems);
+  };
+
+  const handleOnchange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleDelete = (id) => {
+    const newItem = items
+      .filter((item) => item.id !== id)
+      .map((item, index) => {
+        return { ...item, id: index + 1 };
+      });
+    setItems(newItem);
+  };
+
+  const handleAddorSave = () => {
+    if (!isEdit) {
+      setItems([
+        ...items,
+        { id: items.length + 1, label: text, checked: false },
+      ]);
+    } else {
+      const newItem = items.map((item) => {
+        return item.id === currentElementID ? { ...item, label: text } : item;
+      });
+      setItems(newItem);
+      setElementID(null);
+      setIsEdit(false);
+    }
+    setText("");
+  };
+
+  const handleUpdate = (id) => {
+    setIsEdit(true);
+    setElementID(id);
+    const getItem = items.find((item) => item.id === id);
+    setText(getItem.label);
+  };
+
   return (
     <main>
       {/* <h1 style={handingStyle}>Main Content-{name}</h1>
@@ -60,13 +110,36 @@ function Content() {
       </Button> */}
 
       {/* <Shop /> */}
+      <input
+        type="text"
+        placeholder="add someting "
+        value={text}
+        onChange={handleOnchange}
+      ></input>
+      <button onClick={handleAddorSave}>{isEdit ? "Save" : "Add"}</button>
       <ul>
         {items.map((item) => {
           return (
             <li key={item.id}>
-              <input type="checkBox" checked={item.checked}/>
+              <input
+                type="checkBox"
+                checked={item.checked}
+                value={text}
+                onChange={(e) => {
+                  handlechecked(item.id);
+                }}
+              />
               <label>{item.label}</label>
-              <button>Delete</button>
+              <FaEdit
+                role="button"
+                onClick={() => handleUpdate(item.id)}
+                tabIndex={0}
+              />
+              <FaTrashCan
+                role="button"
+                tabIndex={0}
+                onClick={() => handleDelete(item.id)}
+              />
             </li>
           );
         })}
